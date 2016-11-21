5b423c18c10db591c5e512d29ad354d61db5ddb3
shahriar <hsleonis@gmail.com>
2016-11-21 14:33:05 +0600

    Slakbot Jobs

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(3000,
+/*const server = app.listen(3000,
         () => {
             console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);
-        });
+        });*/
 
         app.post('/', (req, res) => {
                 if(typeof req.body!=='undefined')
