3255396c4db6e4110e83228ed62ac37c14495960
shahriar <hsleonis@gmail.com>
2016-11-21 13:18:50 +0600

    Slakbot Jobs

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const cheerio = require('cheerio');
+const request = require('request');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(3000,
+        () => {
+            console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);
+        });
+
+        app.post('/', (req, res) => {
+                let text = req.body.text;
+
+                var url = 'http://jobs.bdjobs.com/jobsearch.asp';
+                var propertiesObject = { txtsearch:text, qOT:'0', hidJobSearch:'jobsearch' };
+
+                request({url:url, qs:propertiesObject}, function(error, response, html){
+
+                    if(!error){
+                        var html = cheerio.load(html);
+
+                        fs.writeFile('output.json', html, function(err){
+
+                        });
+
+                        let data = {
+                            response_type: 'in_channel', // public to the channel
+                            text: html,
+                            attachments:[ {
+                                image_url: 'https://assets.api.ai/openapi-prod/300/assets/img/logo-black.png'
+                            } ]};
+
+                        res.json(data);
+                    }
+                });
+        });
