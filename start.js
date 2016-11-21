35ea04cd52d51a4d1b556d843ee7a0755718fde8
shahriar <hsleonis@gmail.com>
2016-11-21 15:33:34 +0600

    Slakbot Jobs

--- start.js
+++ start.js
@@ -12,6 +12,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
         console.log('Express server   listening on port %d in %s mode', server.address().port,   app.settings.env);
     });
 
+    app.get('/', function(req, res){
+        var data = {
+            response_type: 'in_channel', // public to the channel
+            text: 'Hello World!',
+            attachments:[ {
+                image_url: 'https://assets.api.ai/openapi-prod/300/assets/img/logo-black.png'
+            } ]};
+
+        res.json(data);
+    });
+
     app.post('/', function(req, res){
         if(typeof req.body!=='undefined')
             var text = req.body.text;
@@ -20,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
         var url = 'http://jobs.bdjobs.com/jobsearch.asp';
         var propertiesObject = { txtsearch:text, qOT:'0', hidJobSearch:'jobsearch' };
-
+        console.log('here');
         request({url:url, qs:propertiesObject}, function(error, response, html){
 
             if(!error){
@@ -39,5 +50,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
                 res.json(data);
             }
+            else console.log(error);
         });
     });
